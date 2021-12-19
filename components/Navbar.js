4f4f71129c55fdd43914c28490aafdd575d354a7
Ajdour Hassan <ajdourelhassa2096@gmail.com>
2021-12-19 03:17:17 +0100

    add new files

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import classes from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <div className={classes.container}>
+      <h1> Navbar </h1>
+    </div>
+  );
+}
+
+export default Navbar;
