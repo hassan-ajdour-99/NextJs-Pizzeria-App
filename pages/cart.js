315443db23d4776886be9ab0cff500061b35d936
Ajdour Hassan <ajdourelhassa2096@gmail.com>
2022-01-06 22:18:42 +0100

    add all files

---
+++ pages/cart.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import classes from "../styles/cart.module.css";
+
+function Cart() {
+  return (
+    <div className={classes.container}>
+      <div className={classes.left}>
+        <table className={classes.table}>
+          <tr className={classes.tr}>
+            <th> Product </th>
+            <th> Name </th>
+            <th> Extras </th>
+            <th> Product </th>
+            <th> Quantity </th>
+            <th> Total </th>
+          </tr>
+          <tr>
+            <td>
+              <div className={classes.imageContainer}>
+                <Image
+                  src="/img/pizza2.png"
+                  alt=""
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
+            </td>
+            <td>
+              <span className={classes.name}> Margaerita Pizza </span>
+            </td>
+            <td>
+              <span className={classes.extras}>
+                Extras Ingredience , double spicy .
+              </span>
+            </td>
+            <td>
+              <span className={classes.price}> 19.90$ </span>
+            </td>
+            <td>
+              <span className={classes.quantity}> 3 </span>
+            </td>
+            <td>
+              <span className={classes.total}> 89$ </span>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div className={classes.right}>
+        <div className={classes.wrapper}>
+          <h2 className={classes.title}> CART TOTAL </h2>
+          <div className={classes.totalText}>
+            <b className={classes.totalTextTitle}> Subtotal : </b> up %50
+          </div>
+          <div className={classes.totalText}>
+            <b className={classes.totalTextTitle}> Discount : </b> up %50
+          </div>
+          <div className={classes.totalText}>
+            <b className={classes.totalTextTitle}> TOTAL : </b> up %50
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -3,16 +3,19 @@ import Image from "next/image";
 import Link from "next/link";
 import classes from "../../styles/product.module.css";
 
+const pizza = {
+  id: 1,
+  img: "/img/pizza2.png",
+  name: "Margarita",
+  price: [19.99, 90.99, 89.99],
+  description:
+    "lorem somewhere thta you can only buy this pizza ,lorem somewhere thta you can only buy this pizza, lorem somewhere thta you can only buy this pizza ",
+};
+
 function product() {
-  const [size, setSize] = useState(0);
-  const pizza = {
-    id: 1,
-    img: "/img/pizza2.png",
-    name: "Margarita",
-    price: [19.99, 9.99, 89.99],
-    description:
-      "lorem somewhere thta you can only buy this pizza ,lorem somewhere thta you can only buy this pizza, lorem somewhere thta you can only buy this pizza ",
-  };
+  const [Size, setSize] = useState(1);
+
+  console.log(Size);
   return (
     <div className={classes.container}>
       <div className={classes.left}>
@@ -29,22 +32,65 @@ function product() {
         <h1 className={classes.title}>
           {pizza.name} <span className={classes.spanTitle}> Pizza </span>
         </h1>
-        <span className={classes.price}>${pizza.price[0]}</span>
+        <span className={classes.price}>${pizza.price[Size]}</span>
         <p className={classes.description}>{pizza.description}</p>
         <h3 className={classes.chooseSize}> Choose The Size : </h3>
         <div className={classes.sizes}>
-          <div className={classes.size}>
+          <div className={classes.size} onClick={() => setSize(0)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <span className={classes.number}> small </span>
           </div>
-          <div className={classes.size}>
+          <div className={classes.size} onClick={() => setSize(1)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <span className={classes.number}> Meduim </span>
           </div>
-          <div className={classes.size}>
+          <div className={classes.size} onClick={() => setSize(2)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <span className={classes.number}> Large </span>
           </div>
+        </div>
+        <h3 className={classes.choose}> Choose Additional Inreadience </h3>
+        <div className={classes.ingredience}>
+          <div className={classes.option}>
+            <input
+              type="checkbox"
+              id="double"
+              name="double"
+              className={classes.checkbox}
+            />
+            <label htmlFor="double"> Double Incredience </label>
+          </div>
+          <div className={classes.option}>
+            <input
+              type="checkbox"
+              id="cheese"
+              name="cheese"
+              className={classes.checkbox}
+            />
+            <label htmlFor="spicy"> Extra Chesse </label>
+          </div>
+          <div className={classes.option}>
+            <input
+              type="checkbox"
+              id="garlis"
+              name="garlis"
+              className={classes.checkbox}
+            />
+            <label htmlFor="garlis"> Garlis sousse </label>
+          </div>
+          <div className={classes.option}>
+            <input
+              type="checkbox"
+              id="spicy"
+              name="spicy"
+              className={classes.checkbox}
+            />
+            <label htmlFor="spicy"> Spicy Sousse </label>
+          </div>
+        </div>
+        <div className={classes.add}>
+          <input type="number" defaultValue={1} className={classes.qauntity} />
+          <button className={classes.button}> Add to cart </button>
         </div>
       </div>
     </div>
